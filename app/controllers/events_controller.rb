f981a74936688b078f4717e88c8f1c864296fa5b
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-01-15 12:48:20 +0100

    2.9

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,7 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
+    @event = Event.find(params[:id])
   end
 
   # GET /events/new
@@ -23,8 +24,8 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
+        format.html { redirect_to events_index_path, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: events_index_path }
       else
         format.html { render :new }
         format.json { render json: @event.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root :to => "tickets#index"
+
   get 'events/index'
   get 'events/new'
-  get 'events/create'
-  root :to => "tickets#index"
+  post 'events/create'
+  get 'events/:id' => 'events#show'
 
   resources :tickets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
