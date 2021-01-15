bf06ada5b01e4aaea6bf2bc5a85ca9159f4fd588
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-01-15 12:28:11 +0100

    2.6

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,40 @@
+class EventsController < ApplicationController
+
+  # GET /events
+  # GET /events.json
+  def index
+    @events = Event.all
+  end
+
+  # GET /events/1
+  # GET /events/1.json
+  def show
+  end
+
+  # GET /events/new
+  def new
+    @event = Event.new
+  end
+
+  # POST /events
+  # POST /events.json
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def event_params
+      params.require(:event).permit(:artist, :description, :price_low, :price_high, :event_date)
+    end
+end
