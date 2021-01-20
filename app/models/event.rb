7389007d758273f33c5d8db304f2ac4fe37ba64c
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-01-20 08:34:22 +0100

    2.27

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,25 @@
 class Event < ApplicationRecord
+  # Built in validators
   validates :artist, :presence => true, :length => { minimum: 3 }
   validates :price_low, :presence => true, :numericality => true
   validates :price_high, :presence => true, :numericality => true
   validates :event_date, :presence => true
 
+  # Custom validators
+  validate :price_high_value, :event_not_passed
+
+  # Relations
   has_many :tickets
+
+  def event_not_passed
+    if event_date < Date.today
+      errors.add('Event', 'already passed')
+    end
+  end
+
+  def price_high_value
+    if price_high < price_low
+      errors.add('Price high', "value should be equal to #{price_low} or larger")
+    end
+  end
 end
