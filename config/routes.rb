c30b3c23c2af29685441e5a8685f8ab787e67ab4
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-01-22 12:25:35 +0100

    14.14

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
 
   resources :events, :only => [:index, :new, :create, :show]
   resources :tickets
+
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
